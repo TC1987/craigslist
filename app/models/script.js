be726ff99907915c52fcf8a665765f834f4b0dbb
TSC <tsc7891@gmail.com>
2017-04-04 14:41:11 -0700

    More thorough validiation.

--- app/models/script.js
+++ app/models/script.js
@@ -5,13 +5,39 @@ function submitForm() {
     // error will show up for just the bottom input field making the UX horrible.
 
     // This isn't even a good way to write this either. Should possibly pass in a flag and then have a function that displays all errors?
+    var errorMessage = '';
+
     if (!validateEmail()) {
-        alert('Hey, you\'re missing your email or the format is messed up.');
         error(form.email);
-        return;
+        errorMessage += 'Missing email.\n';
     }
 
-    console.log(form);
+    if (!validatePassword()) {
+        error(form.password);
+        errorMessage += 'Missing password.\n'
+    }
+
+    if (errorMessage) {
+        displayError(errorMessage);
+        return;
+    };
+
+    var data = {
+        email: form.phone.value,
+        password: form.password.value
+    };
+
+    // New to ES6. First arg is the URL that you went to send the data to. Second is the configuration object.
+    // headers: Specifying the data type being sent.
+    // method: The request method.
+    // body: The data being sent. Note that if you want to pass an object, it needs to be converted into JSON first, hence JSON.stringify.
+    fetch('http://localhost:3000/users', {
+        headers: {
+            'Content/Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
 }
 
 function validateEmail() {
@@ -23,14 +49,18 @@ function validateEmail() {
 function validatePassword(password) {
     var password = form.password.value,
         confirmPassword = form.confirmPassword.value;
-
-
 }
 
 function error(target) {
-    target.style.border = '#px solid #F00';
+    target.style.border = '3px solid #F00';
 }
 
 function clearError(target) {
     target.style.border = '';
+}
+
+function displayError(message) {
+    var errorDiv = document.getElementById('error-message');
+    errorDiv.innerHTML = message;
+    errorDiv.style.visibility = 'visible';
 }
